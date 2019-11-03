e47b7da263ab3ef088c559f51b6d14333732699b
Ref.101 <24806887+GeekEast@users.noreply.github.com>
2019-11-03 11:03:47 +1030

    Security Issue

--- src/DB.java
+++ src/DB.java
@@ -4,9 +4,7 @@ import java.sql.SQLException;
 
 public class DB {
     private final String DRIVER = "org.postgresql.Driver";
-    private final String URL = "jdbc:postgresql://ec2-54-235-246-201.compute-1.amazonaws.com:5432/d87uk5edrq565u?sslmode=require&user=afzigcojjloqmn&password=ba817ef45615956f9577c3a34fd7f780905518b5c5cffb8f3ce8c60ee6f57e96";
-    private final String USERNAME = "afzigcojjloqmn";
-    private final String PASSWORD = "ba817ef45615956f9577c3a34fd7f780905518b5c5cffb8f3ce8c60ee6f57e96";
+    private final String URL = "jdbc:postgresql://<address>:<port>/<database>?sslmode=require&user=<username>&password=<password>";
 
     private Connection conn;
 
